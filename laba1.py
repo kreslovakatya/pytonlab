60d8f32d26e6503399089850e1bff2c3974604fa
kreslovakatya <mcat7566@gmail.com>
2023-09-18 15:23:37 +0300

    initial commit

--- laba1.py
+++ laba1.py
@@ -1,1 +1,27 @@
-print("Hellow World")
+# 1 zavd
+a = int(input("a: "))
+b = int(input("b: "))
+x = 0
+if a < b:
+    x = a / b + 1
+elif a == b:
+    x = -1
+else:
+    x = (a * 5 - 1) / a
+
+print("x = " + str(x))
+print()
+#2 zavd
+for i in range(21):
+    if i % 2 == 0:
+        print(i)
+
+print()
+# 3 zavd
+n=5
+
+for i in range(1, n+1):
+    for j in range(5, i - 1, -1):
+        print(j, end='')
+
+    print()
